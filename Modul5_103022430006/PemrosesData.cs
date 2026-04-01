d0089aa3ac85d98ef8e7d2565bf36f5f617db17f
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:30:19 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

---
+++ Modul5_103022430006/PemrosesData.cs
@@ -0,0 +1,31 @@
+﻿
+using Modul5_103022430006;
+using System;
+using System.IO.Pipelines;
+
+namespace Modul5_103022430006
+{
+    public class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            dynamic max = a; // Menggunakan dynamic untuk memungkinkan operasi matematis
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
+        }
+        public static void Main(string[] args)
+        {
+            PemrosesData pemrosesData = new PemrosesData();
+            Console.WriteLine("Nilai terbesar dari 10, 20, dan 15 adalah: " + pemrosesData.DapatkanNilaiTerbesar(10, 20, 15));
+
+        }
+    }
+}
+
