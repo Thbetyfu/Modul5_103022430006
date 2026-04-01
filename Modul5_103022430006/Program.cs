0fbd392251e341a6656b99a24e0dbeb09c76ec8a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:16:49 +0700

    Soal D

--- Modul5_103022430006/Program.cs
+++ Modul5_103022430006/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Tanpa membuat file baru (gunakan file yang dibuat saat membuat project):
+//1.Buatlah sebuah class bernama “PemrosesData”.
+//2. Pada class tersebut, tambahkan sebuah method dengan nama “DapatkanNilaiTerbesar” 
+//yang memiliki tiga parameter generic yang sama yaitu “T”. 
+//3. Method tersebut dapat melakukan pencarian nilai terbesar dari tiga input/argument yang 
+//diberikan pada method tersebut. 
+//4. Hint: Gunakan variable sementara dengan tipe data dynamic untuk memungkinkan 
+//operasi matematis misalnya penjumlahan.
+//5. Hint: Gunakan operator perbandingan untuk menentukan nilai maksimum
+
+using System;
+
+namespace Modul5_103022430006
+{
+    public class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            dynamic max = a; // Menggunakan dynamic untuk memungkinkan operasi matematis
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
+        }
+    }
+}
